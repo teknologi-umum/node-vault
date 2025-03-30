c1d3cc924aba20cc22a726d3f0969579a3107c85
Reinaldy Rafli <aldy505@proton.me>
2025-03-30 12:21:28 +0700

    fix(ssh): include key type on request body

--- src/SSH.ts
+++ src/SSH.ts
@@ -8,7 +8,7 @@ type GenerateSSHCredentialsResponse = {
   data: {
     ip: string;
     key: string;
-    key_type: string;
+    key_type: "otp" | "ca";
     port: number;
     username: string;
   }
@@ -41,6 +41,15 @@ export class SSH {
     if (mountPath === "") throw new ArgumentError("mountPath");
   }
 
+  /**
+   * This endpoint creates credentials for a specific username and IP with the parameters defined in the given role.
+   * @param role Specifies the name of the role to create credentials against.
+   * @param ip Specifies the IP of the remote host.
+   * @param username  Specifies the username on the remote host.
+   * @param signal 
+   * @returns 
+   * @see https://developer.hashicorp.com/vault/api-docs/secret/ssh#generate-ssh-credentials
+   */
   async generateSSHCredentials(role: string, ip: string, username?: string, signal?: AbortSignal): Promise<GenerateSSHCredentialsResponse["data"]> {
     const pathToWrite = `${this.mountPath}/creds/${role}`;
     const body = {
@@ -57,10 +66,18 @@ export class SSH {
     return response.data;
   }
 
-  async listRolesByIp(ip: string, signal?: AbortSignal): Promise<string[]> {
+  /**
+   * This endpoint lists all of the roles with which the given IP is associated.
+   * @param ip Specifies the IP of the remote host.
+   * @param signal 
+   * @returns An array of roles as a secret structure.
+   * @see https://developer.hashicorp.com/vault/api-docs/secret/ssh#list-roles-by-ip
+   */
+  async listRolesByIp(ip: string, keyType: "otp" | "ca", signal?: AbortSignal): Promise<string[]> {
     const pathToWrite = `${this.mountPath}/roles/${ip}`;
     const body = {
-      ip: ip
+      ip: ip,
+      key_type: keyType
     };
 
     const response = await this.client.post<ListRolesByIpResponse>(`v1/${pathToWrite}`, {
@@ -72,6 +89,13 @@ export class SSH {
     return response.data.roles;
   }
 
+  /**
+   * This endpoint verifies if the given OTP is valid. This is an unauthenticated endpoint.
+   * @param otp  Specifies the One-Time-Key that needs to be validated.
+   * @param signal 
+   * @returns 
+   * @see https://developer.hashicorp.com/vault/api-docs/secret/ssh#verify-ssh-otp
+   */
   async verifySSHOTP(otp: string, signal?: AbortSignal): Promise<VerifySSHOTPResponse["data"]> {
     const pathToWrite = `${this.mountPath}/verify`;
     const body = {
